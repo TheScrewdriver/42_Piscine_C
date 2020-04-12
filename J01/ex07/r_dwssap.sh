970e952b1110935a8f224d81fe67e5fa332074f9
Romann Broque <romannbroque@gmail.com>
2020-04-12 17:14:16 +0200

    [J01] ex07 (corrige)

--- J01/ex07/r_dwssap.sh
+++ J01/ex07/r_dwssap.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat /etc/passwd | grep -v "^#^*" | sed -n "n;p" | rev | sort -r | sed -n $FT_LINE1, $FT_LINE2 p | tr "*:" "," | cut -d, -f8 | sed "s/_/_, /" | tr -d "\n" | sed 's/.\{2\}$/./'
+cat /etc/passwd | grep -v "^#^*" | sed -n "n;p" | rev | sort -r | sed -n "$FT_LINE1, $FT_LINE2 p" | tr "*:" "," | cut -d, -f8 | sed "s/_/_, /" | tr -d "\n" | sed 's/.\{2\}$/./'
