f383be6ef52f117dfeb39a282640c8bf749bd7b6
Romann Broque <romannbroque@gmail.com>
2020-04-18 15:09:19 +0200

    [J02] ex05

---
+++ J02/ex05/ft_print_comb2.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbroque <romannbroque@gmail.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/04/16 11:45:05 by rbroque           #+#    #+#             */
+/*   Updated: 2020/04/18 15:08:18 by rbroque          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_list4(int a, int b)
+{
+	ft_putchar((a / 10) + 48);
+	ft_putchar((a % 10) + 48);
+	ft_putchar(' ');
+	ft_putchar((b / 10) + 48);
+	ft_putchar((b % 10) + 48);
+	if (a == 98 && b == 99)
+	{
+		return ;
+	}
+	ft_putchar(',');
+	ft_putchar(' ');
+}
+
+void	ft_print_comb2(void)
+{
+	int		first;
+	int		second;
+
+	first = 0;
+	while (first < 99)
+	{
+		second = first + 1;
+		while (second <= 99)
+		{
+			if (second != first)
+			{
+				ft_list4(first, second);
+				second++;
+			}
+			else if (second == first)
+			{
+				second++;
+			}
+		}
+		first++;
+	}
+}
