363f013b34eb22387c9a78fe4dc4452c552bfacd
BROQUE Romann <romannbroque@gmail.com>
2020-04-26 21:54:54 +0200

    [J03] ex07

--- J03/ex07/ft_strrev.c
+++ J03/ex07/ft_strrev.c
@@ -6,26 +6,39 @@
 /*   By: rbroque <romannbroque@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/23 01:42:09 by rbroque           #+#    #+#             */
-/*   Updated: 2020/04/23 17:31:56 by rbroque          ###   ########.fr       */
+/*   Updated: 2020/04/26 21:52:06 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+void ft_putchar(char c);
+
 char	*ft_strrev(char *str)
 {
-	int		length;
-	char	*temp;
+	int		len;
+	int		i;
+	char	temp;
 
-	length = 0;
-	while (*str)
+	
+	len = 0;
+	i = 0;
+	while (*str != '\0')
 	{
 		str++;
-		length++;
+		len++;
 	}
-	while (length >= 0)
+	while (len > 0)
 	{
-		temp = temp + *str;
-		length--;
+		temp = str[len]
+		str[i] = str[len];
+		len--;
+		i++;
 	}
-	str = temp;
 	return (str);
+}
+
+int		main(void)
+{
+	ft_strrev("Bonjour");
+	ft_putchar('h');
+	return(0);
 }
