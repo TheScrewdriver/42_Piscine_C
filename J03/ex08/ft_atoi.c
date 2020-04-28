ec24a3ff20222ff0c00566768aa9d0c573d07ff6
BROQUE Romann <romannbroque@gmail.com>
2020-04-28 19:45:44 +0200

    [J03] ex08 - finished and normed (without long exception)

--- J03/ex08/ft_atoi.c
+++ J03/ex08/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: rbroque <romannbroque@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/27 09:08:53 by rbroque           #+#    #+#             */
-/*   Updated: 2020/04/28 19:18:22 by rbroque          ###   ########.fr       */
+/*   Updated: 2020/04/28 19:43:32 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,9 @@ int		ft_atoi(char *str)
 	if (str[i] == '-' || str[i] == '+')
 		i++;
 	while (str[i] != '\0' && str[i] >= '0' && str[i] <= '9')
-		{
-			result = result * 10 + str[i] - '0';
-			i++;
-		}
-	return (result = (str[0] == '-')? -result : result);
+	{
+		result = result * 10 + str[i] - '0';
+		i++;
+	}
+	return (result = (str[0] == '-') ? -result : result);
 }
