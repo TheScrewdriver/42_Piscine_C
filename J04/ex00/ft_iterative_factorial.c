4a4c51d4daf668a9329fff8395275f47b1b95f86
BROQUE Romann <romannbroque@gmail.com>
2020-04-30 12:04:33 +0200

    [J04] ex00

--- J04/ex00/ft_iterative_factorial.c
+++ J04/ex00/ft_iterative_factorial.c
@@ -1,40 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_sort_integer_table.c                            :+:      :+:    :+:   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rbroque <romannbroque@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/04/28 19:20:54 by rbroque           #+#    #+#             */
-/*   Updated: 2020/04/30 11:02:40 by rbroque          ###   ########.fr       */
+/*   Created: 2020/04/30 11:08:38 by rbroque           #+#    #+#             */
+/*   Updated: 2020/04/30 12:02:46 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_sort_integer_table(int *tab, int size)
+int		ft_iterative_factorial(int n)
 {
 	int		i;
 	int		tmp;
-	int		complete;
 
-	complete = 1;
 	i = 0;
-	while (complete != 0)
+	tmp = 1;
+	if (n <= 12 && n >= 0)
 	{
-		if (tab[i] > tab[i + 1] && i + 1 < size)
+		while (i < n)
 		{
-			tmp = tab[i + 1];
-			tab[i + 1] = tab[i];
-			tab[i] = tmp;
-			complete++;
-		}
-		if (i == size - 1 && complete == 1)
-			complete = 0;
-		else if (i == size - 1)
-		{
-			i = 0;
-			complete = 1;
-		}
-		else if (i < size && complete != 0)
 			i++;
+			tmp = i * tmp;
+		}
+		n = tmp;
+		return (n);
 	}
+	else
+		return (0);
 }
