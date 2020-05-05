805610f94f58e4a75e9e2c5645a684ef3c39f6c9
BROQUE Romann <romannbroque@gmail.com>
2020-05-05 10:10:25 +0200

    [J04] ex07

--- J04/ex07/ft_find_next_prime.c
+++ J04/ex07/ft_find_next_prime.c
@@ -1,27 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_is_prime.c                                      :+:      :+:    :+:   */
+/*   ft_find_next_prime.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rbroque <romannbroque@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/05/04 17:01:58 by rbroque           #+#    #+#             */
-/*   Updated: 2020/05/04 22:20:37 by rbroque          ###   ########.fr       */
+/*   Created: 2020/05/04 22:11:20 by rbroque           #+#    #+#             */
+/*   Updated: 2020/05/05 10:07:00 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <limits.h>
 
-int		ft_is_prime(int nb)
+int		ft_find_next_prime(int nb)
 {
-	int		div;
-
-	div = 2;
-	if (nb <= 1 || nb > INT_MAX)
+	if (nb >= INT_MAX)
 		return (0);
-	while (nb % div != 0 && div + 1 < nb)
-		div++;
-	if (nb % div == 0)
-		return (0);
-	return (1);
+	if (nb == 1 || nb < 0)
+		return (2);
+	if (nb % 2 == 0 && nb != 2)
+		return (ft_find_next_prime(nb + 1));
+	if (nb % 3 == 0 && nb != 3)
+		return (ft_find_next_prime(nb + 1));
+	if (nb % 5 == 0 && nb != 5)
+		return (ft_find_next_prime(nb + 1));
+	if (nb % 7 == 0 && nb != 7)
+		return (ft_find_next_prime(nb + 1));
+	return (nb);
 }
