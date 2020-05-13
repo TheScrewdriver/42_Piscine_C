d40ca85b17edf12fb3450c0bc69c2042da50735e
BROQUE Romann <romannbroque@gmail.com>
2020-05-13 14:43:37 +0200

    [J05] ex00

--- J05/ex00/ft_strcpy.c
+++ J05/ex00/ft_strcpy.c
@@ -6,47 +6,20 @@
 /*   By: rbroque <romannbroque@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/11 11:38:27 by rbroque           #+#    #+#             */
-/*   Updated: 2020/05/12 23:30:20 by rbroque          ###   ########.fr       */
+/*   Updated: 2020/05/13 14:43:04 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
-#include <stdio.h>
-
-void	ft_putstr(char *str)
+char *ft_strcpy(char *dest, char *src)
 {
 	int		i;
 
 	i = 0;
-	while (str[i] != '\0')
+	while (src[i] != '\0')
 	{
-		printf("%c", str[i]);
+		dest[i] = src[i];
 		i++;
 	}
-}
-
-char *ft_strcpy(char *dest, char *str)
-{
-	int		i;
-
-	i = 0;
-	while (str[i] != '\0')
-	{
-		dest[i] = str[i];
-		i++;
-	}
+	dest[i] = '\0';
 	return (dest);
-}
-
-int		main(int argc, char *argv[])
-{
-	if (argc != 3)
-		return (0);
-	printf("%s", ft_strcpy(argv[argc],argv[argc - 1] ));
-	printf("%c", '\n');
-	ft_putstr(argv[argc]);
-	printf("%c", '\n');
-	ft_putstr(argv[argc - 1]);
-	
-	return (0);
 }
